1ccf3f319398682abf4834aa1f6a57b61d2eee3c
lmswxy <lmswxy@163.com>
2017-05-09 23:23:11 +0800

    update some info about the file operation
    continue to do the report generate

--- ExcelFileReport.py
+++ ExcelFileReport.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import panda as pd
+import pandas as pd
 import matplotlib as mp
 import xlrd
 import xlwt
@@ -11,7 +11,7 @@ import time
 import getopt
 import subprocess
 import threading
-import string
+import optparse
 
 PARAM_DUT                      = 'HAWKEYE'
 PARAM_TEST_PLAN_COVER          = 'Testplan Cover'
@@ -28,3 +28,39 @@ PARAM_TEST_RESULT_NOTRUN       = 'Not Run'
 PARAM_TEST_RESULT_UNDERRUN     = 'Under Run'
 PARAM_TEST_RESULT_NA           = 'NA'
 
+def Search_File(FileName, SearchPath, PathSep=os.pathsep):
+    for path in SearchPath.split(os.pathsep):
+        candidate = os.path.join(path, FileName)
+        if os.path.isfile(candidate):
+            yield os.path.abspath(candidate)
+
+def FileExist(FileName, SearchPath):
+    find_file = list(Search_File(FileName, SearchPath))
+    if find_file:
+	return True
+    else:
+	return False
+
+def PlatformInfo():
+    if platform.platform()[:3].lower() == 'win':
+	return 'Windows'
+    elif platform.platform()[:3].lower() == 'lin':
+	return 'Linux'
+    else:
+	pass
+
+def MarkTestResulttoTestPlan(ResultFileName, TestPlanName):
+    
+def parse_args():
+    usage = u'''To Find a file, the 1st para is path, and the 2nd para is file name'''
+    parser = optparse.OptionParser(usage)
+    help = u'The target file name'
+    parser.add_option('--filename', help=help)
+    help = u'multi path which seperate via ',''
+    parser.add_option('--path', help=help, default='e:')
+    options, args = parser.parse_args()
+    return options, args
+
+if __name__ == '__main__':
+    print FileExist('Hawkeye_WiFi_SoC_VI_Testplan.xlsx',os.getcwd())
+    print PlatformInfo()
